27132af417568fedf323a386c619d66dd5c4ece8
MalyHovad <jakub.dalecky@gmail.com>
2026-03-29 01:37:30 +0100

    Holy revamp

---
+++ src/components/FileTree.tsx
@@ -0,0 +1,9 @@
+export function FileTree() {
+    return (
+    <section id="file-tree">
+        <img id="tempkoncept" src="src\assets\koncept_filething.png" alt="koncept filetree"/>
+        <br />
+        viz koncept art výše :3
+    </section>
+    )
+}
---
+++ src/components/Footer.tsx
@@ -0,0 +1,27 @@
+export function Footer() {
+  return (
+    <section id = "footer">
+        <div id="authors-nsc">
+          <h2>Nuclear Simulations Classroom</h2>
+          <p>Interested in nuclear energetics? <br />NSC is a very good starting point.</p>
+          <ul>
+            <li>
+              <a href="https://discord.gg/Wek3pzab3r" target="_blank">
+                NSC Discord
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="authors-ea">
+          <h2>E-Atom</h2>
+          <p>Building the Temelín NPP in both Roblox and Godot. Check them out!</p>
+          <ul>
+            <li>
+              <a href="https://discord.gg/Z6z7WpqbWD" target="_blank">
+                E-Atom Discord Server
+              </a>
+            </li>
+          </ul>
+        </div>
+    </section>)
+}
